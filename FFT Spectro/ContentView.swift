e60311e8dee9b9414b7931f5376918401b8e6d13
Anadyr <anadyre@icloud.com>
2020-05-24 16:43:16 -0800

    Initial Commit

---
+++ FFT Spectro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FFT Spectro
+//
+//  Created by Anadyr on 5/24/20.
+//  Copyright © 2020 Anadyr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
